fa1a6ad6704393b117c6f93936e565ea0226df2f
李中强 <328656732@qq.com>
2017-12-06 17:56:05 +0800

    add tabbar

---
+++ ScanCode/ScanCode/Base/ViewController/GFTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  GFTabBarController.h
+//  ScanCode
+//
+//  Created by lzq-psylife on 2017/12/6.
+//  Copyright © 2017年 lizhongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFTabBarController : UITabBarController
+
+@end
---
+++ ScanCode/ScanCode/Base/ViewController/GFTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  GFTabBarController.m
+//  ScanCode
+//
+//  Created by lzq-psylife on 2017/12/6.
+//  Copyright © 2017年 lizhongqiang. All rights reserved.
+//
+
+#import "GFTabBarController.h"
+
+@interface GFTabBarController ()
+
+@end
+
+@implementation GFTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
